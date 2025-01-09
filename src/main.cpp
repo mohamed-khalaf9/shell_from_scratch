1f4f6164fedc96ad25c31b3dbda85ba0a4458f29
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2025-01-09 22:54:45 +0200

    feat: Add command and argument parsing for user input

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,28 @@
 #include <iostream>
 #include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sstream>
 
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
-
+  
   // Uncomment this block to pass the first stage
+  std::string input;
+  std::stringstream ss(input);
   while(true){
   std::cout << "$ ";
-  std::string input;
-  std::getline(std::cin, input);
-  std::cout <<input+": command not found\n";
+  std::getline(std::cin,input);
+
+  std::string command;
+  int argument;
+  ss >> command >> argument;
+  
+  std::getline(std::cin,input);
+  std::cout << command+": command not found\n";
+
+
   }
- 
 }
