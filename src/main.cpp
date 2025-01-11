cf1883b3cd2c4d0d4303b053b9901ff197fc28a2
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2025-01-11 21:37:14 +0200

    fix: modify command passed to system function to just be full path plus programm argument to fix error in tests

--- src/main.cpp
+++ src/main.cpp
@@ -121,7 +121,7 @@ int main() {
     std::string full_path = is_executable_file_exists_in_path(programm_name);
     if(full_path!="")
     {
-      std::string command = full_path + " " + programm_name + " " + programm_argument;
+      std::string command = full_path + " " + programm_argument;
       system(command.c_str());
 
     }
