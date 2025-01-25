a23b831464565f71dbde1af3e15f55e62625a25e
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2025-01-25 23:48:02 +0200

    declare function trim

--- src/helpers.h
+++ src/helpers.h
@@ -23,5 +23,6 @@ std::vector<std::string> handle_double_quotes(const std::string& argument);
 std::string handle_non_quoted_backslash(const std::string& argument);
 std::string remove_extra_spaces(const std::string& str);
 std::unordered_map<std::string,std::string> load_commands();
+std::string trim(const std::string &str);
 
 #endif
