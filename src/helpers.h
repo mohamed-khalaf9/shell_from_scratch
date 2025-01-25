f9822e16e88ca80d8c32de4571040461539a91dc
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2025-01-25 23:42:08 +0200

    declare quoting feature helper functions

--- src/helpers.h
+++ src/helpers.h
@@ -17,6 +17,10 @@ void add_executables_to_trie(Trie& trie);
 std::vector<std::string> split(const std::string &s, char delimiter);
 bool is_path_exist(const std::string& path);
 std::pair<std::string,std::string> fetch_command_and_argument(const std::string& input);
-
+std::vector<std::string> handle_quoting(std::string argument);
+std::vector<std::string> handle_single_qoutes(const std::string & argument);
+std::vector<std::string> handle_double_quotes(const std::string& argument);
+std::string handle_non_quoted_backslash(const std::string& argument);
+std::string remove_extra_spaces(const std::string& str);
 
 #endif
