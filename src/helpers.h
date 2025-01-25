ea3685242b66b90c26689329f56c7f69fd6d8294
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2025-01-25 23:17:00 +0200

    add helper functions for autocompleteion feature

--- src/helpers.h
+++ src/helpers.h
@@ -1,9 +1,21 @@
 #ifndef HELPERS_H
 #define HELPERS_H
+#include <iostream>
+#include <string>
+#include <vector>
+#include <conio.h>
+#include "trie.h"
+#include <filesystem>
+#include <fstream>
+#include <sstream>
+#include <stack>
+#include <algorithm>
 
+std::string parse_input_with_autocomplete(std::string& input,Trie& trie,bool first_time = true);
+void autocomplete(std::string& input, Trie& trie);
+void add_executables_to_trie(Trie& trie);
+std::vector<std::string> split(const std::string &s, char delimiter);
+bool is_path_exist(const std::string& path);
 
-
-
-void hello();
 
 #endif
