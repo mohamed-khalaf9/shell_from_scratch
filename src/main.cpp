ee70d00055605b019174af19653966bd58322cc3
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2025-01-25 13:55:49 +0200

    feat: build trie to find commands with specific perfixes in o(L) time , to use it with autocompletion feature

--- src/main.cpp
+++ src/main.cpp
@@ -572,13 +572,66 @@ std::pair<std::string,std::streambuf*> handle_redirection(const std::string& op,
 
 }
 
+struct TrieNode {
+    std::unordered_map<char, TrieNode*> children; 
+    bool isEndOfWord = false; 
+};
 
+class Trie {
+private:
+    TrieNode* root;
+
+    
+    void collectSuggestions(TrieNode* node, std::string prefix, std::vector<std::string>& suggestions) {
+        if (node->isEndOfWord) {
+            suggestions.push_back(prefix);
+        }
+        for (auto& child : node->children) {
+            collectSuggestions(child.second, prefix + child.first, suggestions);
+        }
+    }
+
+public:
+    Trie() {
+        root = new TrieNode();
+    }
+
+    
+    void insert(const std::string& word) {
+        TrieNode* current = root;
+        for (char ch : word) {
+            if (current->children.find(ch) == current->children.end()) {
+                current->children[ch] = new TrieNode();
+            }
+            current = current->children[ch];
+        }
+        current->isEndOfWord = true;
+    }
+
+    
+    std::vector<std::string> searchPrefix(const std::string& prefix) {
+        TrieNode* current = root;
+        for (char ch : prefix) {
+            if (current->children.find(ch) == current->children.end()) {
+                return {}; 
+            }
+            current = current->children[ch];
+        }
+
+        
+        std::vector<std::string> suggestions;
+        collectSuggestions(current, prefix, suggestions);
+        return suggestions;
+    }
+};
 
 int main()
 {
  
 std::streambuf* cout_original_buf = std::cout.rdbuf(); 
 std::streambuf* cerr_original_buf = std::cerr.rdbuf(); 
+
+
 
 
   
